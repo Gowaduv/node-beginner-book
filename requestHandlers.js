ddf5675ef1dd676316711501126b26f8c236f8a1
KMSkelton <kmskelton@outlook.com>
2016-04-12 00:15:00 -0700

    image is uploaded via /start and displayed at /upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,8 @@
-const exec = require("child_process").exec;
 const querystring = require("querystring");
 const fs = require("fs");
+const formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
   console.log("Request handler for start was called.");
 
   var body = '<!DOCTYPE = html>' +
@@ -12,9 +12,9 @@ function start(response, postData) {
     'charset=utf-8" />' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data" ' + 'method="post">' +
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -26,15 +26,30 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler for upload was called.");
-  response.writeHead(200, {
-    "Content-Type": "text/plain"
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse...");
+form.parse(request, function(error, fields, files) {
+  console.log("parsing done");
+
+  fs.rename(files.upload.path, "./tmp/test.png", function(error) {
+    if (error) {
+      fs.unlink("./tmp/test.png");
+      fs.rename(files.upload.path, "./tmp/test.png")
+    }
   });
-  response.write("You've sent the text: " + querystring.parse(postData).text);
+  response.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  response.write("Received image: <br />");
+  response.write("<img src='/show' />")
   response.end();
+});
 }
-function show(response){
+
+function show(response) {
   console.log("Request handler for show was called.");
   response.writeHead(200, {
     "Content-Type": "image/png"
