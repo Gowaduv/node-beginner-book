30c54fa0cd1ff85e78d966e9f97fefd385956ecd
KMSkelton <kmskelton@outlook.com>
2016-04-11 23:11:34 -0700

    working my way through... at page 40

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,35 @@
 const exec = require("child_process").exec;
 
-function start() {
-  console.log("Request handler 'start' was called.");
-var content = "empty";
+function start(response) {
+  console.log("Request handler for start was called.");
 
-exec("ls -lah", (error, stdout, stderr) => {
-  content = stdout;
-});
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
 
-  return content;
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.write(body);
+  response.end();
 }
 
-function upload(){
-  console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+function upload(response) {
+  console.log("Request handler for upload was called.");
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
